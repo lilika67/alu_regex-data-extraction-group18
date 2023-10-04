2dfacf140e9905b0c766b6dee5ee362311dc29ae
Komaiya Samuel <s.komaiya@alustudent.com>
2023-10-04 15:02:18 -0700

    task completed

---
+++ Restaurant.py
@@ -0,0 +1,16 @@
+import re
+
+# Open the Name-Cuisine.txt file for reading
+with open("/alu_regex-data-extraction-group18/texts/ingredient.txt", "r") as f:
+    # Read the entire file contents into a string
+    text = f.read()
+
+# Compile the regular expression for extracting restaurant names and ingredient
+name_cuisine_regex = re.compile(r"(?P<name>\w+(?:\s+\w+)*)\s+-\s+(?P<cuisine>\w+)")
+
+# Extract all of the restaurant names and cuisines from the text
+name_cuisine_lists = name_cuisine_regex.findall(text)
+
+# Print the extracted restaurant names and ingredient
+for name_cuisine_list in name_cuisine_lists:
+    print(f"{name_cuisine_list[0]}: {name_cuisine_list[1]}")
